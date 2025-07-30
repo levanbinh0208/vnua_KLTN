fd1a18d2ff1d68682eac74259c8935576b9bdb60
le.van.binh <levanbinh020802@gmail.com>
2025-07-30 14:44:52 +0700

    font end login

--- src/main/java/com/vnua/controller/LoginController.java
+++ src/main/java/com/vnua/controller/LoginController.java
@@ -1,24 +1,13 @@
 package com.vnua.controller;
 
-import com.vnua.model.CanBo;
-import com.vnua.service.CanBoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
-public class CanBoController {
+public class LoginController {
 
-    @Autowired
-    private CanBoService canBoService;
-
-    @GetMapping("/canbo")
-    public String list(Model model) {
-        List<CanBo> list = canBoService.getAllCanBo();
-        model.addAttribute("list", list);
-        return "canbo/list";
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login"; // sẽ tìm file templates/login.html
     }
 }
